5fa93f01283a44160a2a07fdfd15cb845692d4a1
Duygu ÇOBAN <74262025+dygcbn@users.noreply.github.com>
2021-01-07 17:50:49 +0300

    Add files via upload

--- player.py
+++ player.py
@@ -5,14 +5,14 @@ from PyQt5.QtCore import QSize,Qt,QTimer
 import random,time
 from pygame import mixer
 from mutagen.mp3 import MP3
-import style
 
 
-musicList=[]
+
+muzikList=[]
 mixer.init()
 muted=False
-count=0
-songLength=0
+sayac=0
+parcauzunluk=0
 index=0
 
 
@@ -23,274 +23,278 @@ class Player(QWidget):
         super().__init__()
         self.setWindowTitle("Müzik Çalar")
         self.setGeometry(450,180,750,700)
-        self.UI()
         self.show()
+        self.widgets()
+        self.layouts()
 
-    def UI(self):
-       self.widgets()
-       self.layouts()
+    
 
     def widgets(self):
         ########################progress bar#############
         self.progressBar=QProgressBar()
-        self.progressBar.setTextVisible(False)
-        self.progressBar.setStyleSheet(style.progressBarStyle())
-        ########################Labels###################
-        self.songTimerLabel=QLabel("0:00")
-        self.songLenthLabel=QLabel("/ 0:00")
-        #######################Buttons###################
+        self.progressBar.setStyleSheet("""
+        QProgressBar {
+        border: 1px solid #bbb;
+        background: white;
+        height: 10px;
+        border-radius: 6px;     
+        }
+    """)
+
+        self.parcauzunluk=QLabel("0:00")
+        self.parcalabel=QLabel("/ 0:00")
+        #######################Butonlar###################
         self.ekle =QToolButton()
-        self.ekle.setIcon(QIcon("icons/add.png"))
+        self.ekle.setIcon(QIcon("butonlar/ekle.png"))
         self.ekle.setIconSize(QSize(48,48))
         self.ekle.setToolTip("Müzik Ekle")
-        self.ekle.clicked.connect(self.addSound)
+        self.ekle.clicked.connect(self.sesekle)
 
-        self.shuffleButton=QToolButton()
-        self.shuffleButton.setIcon(QIcon("icons/shuffle.png"))
-        self.shuffleButton.setIconSize(QSize(48,48))
-        self.shuffleButton.setToolTip("Listeyi Karıştır")
-        self.shuffleButton.clicked.connect(self.shufflePlayList)
+        self.butonkaristir=QToolButton()
+        self.butonkaristir.setIcon(QIcon("butonlar/karistir.png"))
+        self.butonkaristir.setIconSize(QSize(48,48))
+        self.butonkaristir.setToolTip("Listeyi Karıştır")
+        self.butonkaristir.clicked.connect(self.listekaristir)
 
-        self.previousButton = QToolButton()
-        self.previousButton.setIcon(QIcon("icons/previous.png"))
-        self.previousButton.setIconSize(QSize(48, 48))
-        self.previousButton.setToolTip("Önceki Parça")
-        self.previousButton.clicked.connect(self.playPrevious)
+        self.oncekiparca = QToolButton()
+        self.oncekiparca.setIcon(QIcon("butonlar/onceki.png"))
+        self.oncekiparca.setIconSize(QSize(48, 48))
+        self.oncekiparca.setToolTip("Önceki Parça")
+        self.oncekiparca.clicked.connect(self.oncekinical)
 
 
-        self.playButton = QToolButton()
-        self.playButton.setIcon(QIcon("icons/play.png"))
-        self.playButton.setIconSize(QSize(64, 64))
-        self.playButton.setToolTip("Oynat")
-        self.playButton.clicked.connect(self.playSounds)
+        self.oynat = QToolButton()
+        self.oynat.setIcon(QIcon("butonlar/play.png"))
+        self.oynat.setIconSize(QSize(64, 64))
+        self.oynat.setToolTip("Oynat")
+        self.oynat.clicked.connect(self.seslerical)
         
+
+        self.sonrakiparca = QToolButton()
+        self.sonrakiparca.setIcon(QIcon("butonlar/sonraki.png"))
+        self.sonrakiparca.setIconSize(QSize(48, 48))
+        self.sonrakiparca.setToolTip("Sonraki Parça")
+        self.sonrakiparca.clicked.connect(self.sonrakinical)
+
+
+        self.sessiz = QToolButton()
+        self.sessiz.setIcon(QIcon("butonlar/mute.png"))
+        self.sessiz.setIconSize(QSize(24, 24))
+        self.sessiz.setToolTip("Sessiz")
+        self.sessiz.clicked.connect(self.sesikapat)
         
+        self.muzikekle =QToolButton()
+        self.muzikekle.setIcon(QIcon("butonlar/ekle.png"))
+        self.muzikekle.setIconSize(QSize(48,48))
+        self.muzikekle.setToolTip("Müzik ekle")
+        self.muzikekle.clicked.connect(self.sesekle)
 
-        self.nextButton = QToolButton()
-        self.nextButton.setIcon(QIcon("icons/next.png"))
-        self.nextButton.setIconSize(QSize(48, 48))
-        self.nextButton.setToolTip("Sonraki Parça")
-        self.nextButton.clicked.connect(self.playNext)
+        #####################ses Slider#################
+        self.sesslider=QSlider(Qt.Horizontal)
+        self.sesslider.setToolTip("Ses")
+        self.sesslider.setValue(70)
+        self.sesslider.setMinimum(0)
+        self.sesslider.setMaximum(100)
 
+        self.sesslider.valueChanged.connect(self.sesayarla)
 
-        self.muteButton = QToolButton()
-        self.muteButton.setIcon(QIcon("icons/mute.png"))
-        self.muteButton.setIconSize(QSize(24, 24))
-        self.muteButton.setToolTip("Sessiz")
-        self.muteButton.clicked.connect(self.muteSound)
-        
-        self.addButton =QToolButton()
-        self.addButton.setIcon(QIcon("icons/add.png"))
-        self.addButton.setIconSize(QSize(48,48))
-        self.addButton.setToolTip("Müzik ekle")
-        self.addButton.clicked.connect(self.addSound)
+        ###################oynatma Listesi####################
+        self.oynatmalistesi=QListWidget()
+        self.oynatmalistesi.doubleClicked.connect(self.seslerical)
+        self.oynatmalistesi.setStyleSheet("""
+        QListWidget{
+        background-color:#fff;
+        border-radius: 10px;
+        border:3px solid blue;     
+        }
+    """)
 
-        #####################Volume Slider#################
-        self.volumeSlider=QSlider(Qt.Horizontal)
-        self.volumeSlider.setToolTip("Ses")
-        self.volumeSlider.setValue(70)
-        self.volumeSlider.setMinimum(0)
-        self.volumeSlider.setMaximum(100)
-        mixer.music.set_volume(0.7)
-        self.volumeSlider.valueChanged.connect(self.setVolume)
-
-        ###################Play List####################
-        self.playList=QListWidget()
-        self.playList.doubleClicked.connect(self.playSounds)
-        self.playList.setStyleSheet(style.playListStyle())
-
-        #####################Timer######################
+        #####################zamanlayıcı######################
         self.timer=QTimer()
         self.timer.setInterval(1000)
-        self.timer.timeout.connect(self.updateProgressBar)
+        self.timer.timeout.connect(self.progressbarguncelle)
         
-#######################################################kljhkjhkhkjh
 
 
 
 
     def layouts(self):
-        #########################Creating Layouts#################
-        self.mainLayout=QVBoxLayout()
-        self.topMainLayout=QVBoxLayout()
-        self.topGroupBox=QGroupBox("Müzik Çalar")
-        self.topGroupBox.setStyleSheet(style.groupboxStyle())
-        self.topLayout=QHBoxLayout()
-        self.middleLayout=QHBoxLayout()
-        self.bottomLayout=QVBoxLayout()
+        #########################başlık#################
+        self.anapencere=QVBoxLayout()
+        self.anapencere2=QVBoxLayout()
+        
+        self.penceregrup=QGroupBox("Müzik Çalar")
+        self.penceregrup.setStyleSheet("""
+        QGroupBox {
+        background-color:#acc324;
+        font:15pt Times Bold;
+        color:white;
+        border:2px solid gray;
+        border-radius:15px;
+        }
+    """)
+        self.ustkutu=QHBoxLayout()
+        self.ortakutu=QHBoxLayout()
+        self.altkutu=QVBoxLayout()
 
-        ###################Adding Widgets#########################
-        ##################Top layout widgets######################
-        self.topLayout.addWidget(self.progressBar)
-        self.topLayout.addWidget(self.songTimerLabel)
-        self.topLayout.addWidget(self.songLenthLabel)
+        ###################Widget ekle#########################
+        ##################tepe######################
+        self.ustkutu.addWidget(self.progressBar)
+        self.ustkutu.addWidget(self.parcauzunluk)
+        self.ustkutu.addWidget(self.parcalabel)
 
-        ##################Middle layout Widget#################
-        self.middleLayout.addStretch()
-        self.middleLayout.addWidget(self.addButton)
-        self.middleLayout.addWidget(self.shuffleButton)
-        self.middleLayout.addWidget(self.playButton)
-        self.middleLayout.addWidget(self.previousButton)
-        self.middleLayout.addWidget(self.nextButton)
-        self.middleLayout.addWidget(self.volumeSlider)
-        self.middleLayout.addWidget(self.muteButton)
-        self.middleLayout.addWidget(self.addButton)
-
-        self.middleLayout.addStretch()
-
-        ###################Bottom layout widget#############
-        self.bottomLayout.addWidget(self.playList)
-
-        self.topMainLayout.addLayout(self.topLayout)
-        self.topMainLayout.addLayout(self.middleLayout)
-        self.topGroupBox.setLayout(self.topMainLayout)
-        self.mainLayout.addWidget(self.topGroupBox,25)
-        self.mainLayout.addLayout(self.bottomLayout,75)
-        self.setLayout(self.mainLayout)
-
-    def addSound(self):
-        directory=QFileDialog.getOpenFileName(self,"Müzik Ekle","","Ses Dosyaları (*.mp3 *.ogg *.wav)")
-        # print(directory)
-        filename=os.path.basename(directory[0])
-        # print(filename)
-        self.playList.addItem(filename)
-        musicList.append(directory[0])
-
-    def shufflePlayList(self):
-        random.shuffle(musicList)
-        print(musicList)
-        self.playList.clear()
-        for song in musicList:
-            filename=os.path.basename(song)
-            self.playList.addItem(filename)
-
-    def pause(self):
-        random.shuffle(musicList)
-        print(musicList)
-        self.playList.clear()
-        for song in musicList:
-            filename=os.path.basename(song)
-            self.playList.addItem(filename)
+        ##################orta#################
+        self.ortakutu.addStretch()
+        self.ortakutu.addWidget(self.muzikekle)
+        self.ortakutu.addWidget(self.butonkaristir)
+        self.ortakutu.addWidget(self.oynat)
+        self.ortakutu.addWidget(self.oncekiparca)
+        self.ortakutu.addWidget(self.sonrakiparca)
+        self.ortakutu.addWidget(self.sesslider)
+        self.ortakutu.addWidget(self.sessiz)
 
 
-    def playSounds(self):
-        global songLength
-        global count
+        self.ortakutu.addStretch()
+
+        ###################alt#############
+        self.altkutu.addWidget(self.oynatmalistesi)
+
+        self.anapencere.addLayout(self.ustkutu)
+        self.anapencere.addLayout(self.ortakutu)
+        self.penceregrup.setLayout(self.anapencere)
+        self.anapencere2.addWidget(self.penceregrup,25)
+        self.anapencere2.addLayout(self.altkutu,75)
+        self.setLayout(self.anapencere2)
+
+    def sesekle(self):
+        klasor=QFileDialog.getOpenFileName(self,"Müzik Ekle","","Ses Dosyaları (*.mp3 *.ogg *.wav)")
+        dosya=os.path.basename(klasor[0])
+        self.oynatmalistesi.addItem(dosya)
+        muzikList.append(klasor[0])
+
+    def listekaristir(self):
+        random.shuffle(muzikList)
+        print(muzikList)
+        self.oynatmalistesi.clear()
+        for parca in muzikList:
+            dosya=os.path.basename(parca)
+            self.oynatmalistesi.addItem(dosya)
+
+
+
+    def seslerical(self):
+        global parcauzunluk
+        global sayac
         global index
-        count=0
-        index=self.playList.currentRow()
+        sayac=0
+        index=self.oynatmalistesi.currentRow()
 
         try:
-            mixer.music.load(str(musicList[index]))
+            mixer.music.load(str(muzikList[index]))
             mixer.music.play()
             self.timer.start()
-            sound=MP3(str(musicList[index]))
-            songLength=sound.info.length
-            songLength=round(songLength)
-            print(songLength)
-            min,sec=divmod(songLength,60)
+            sound=MP3(str(muzikList[index]))
+            parcauzunluk=sound.info.length
+            parcauzunluk=round(parcauzunluk)
+            print(parcauzunluk)
+            min,sec=divmod(parcauzunluk,60)
 
-            self.songLenthLabel.setText("/ "+str(min)+":"+str(sec))
+            self.parcalabel.setText("/ "+str(min)+":"+str(sec))
             self.progressBar.setValue(0)
-            self.progressBar.setMaximum(songLength)
+            self.progressBar.setMaximum(parcauzunluk)
 
         except:
             pass
 
-    def playPrevious(self):
-        global songLength
-        global count
+    def oncekinical(self):
+        global parcauzunluk
+        global sayac
         global index
-        count = 0
-        items=self.playList.count()
+        sayac = 0
+        ogeler=self.oynatmalistesi.count()
 
         if index == 0:
-             index = items
+             index = ogeler
         index -=1
 
         try:
-            mixer.music.load(str(musicList[index]))
+            mixer.music.load(str(muzikList[index]))
             mixer.music.play()
             self.timer.start()
-            sound = MP3(str(musicList[index]))
-            songLength = sound.info.length
-            songLength = round(songLength)
-            print(songLength)
-            min, sec = divmod(songLength, 60)
+            sound = MP3(str(muzikList[index]))
+            parcauzunluk = sound.info.length
+            parcauzunluk = round(parcauzunluk)
+            print(parcauzunluk)
+            min, sec = divmod(parcauzunluk, 60)
 
-            self.songLenthLabel.setText("/ " + str(min) + ":" + str(sec))
+            self.parcalabel.setText("/ " + str(min) + ":" + str(sec))
             self.progressBar.setValue(0)
-            self.progressBar.setMaximum(songLength)
-
+            self.progressBar.setMaximum(parcauzunluk)
         except:
             pass
 
-    def playNext(self):
-        global songLength
-        global count
+    def sonrakinical(self):
+        global parcauzunluk
+        global sayac
         global index
-        count = 0
-        items = self.playList.count()
+        sayac = 0
+        ogeler = self.oynatmalistesi.count()
         index += 1
 
-        if index == items:
+        if index == ogeler:
             index = 0
 
 
         try:
-            mixer.music.load(str(musicList[index]))
+            mixer.music.load(str(muzikList[index]))
             mixer.music.play()
             self.timer.start()
-            sound = MP3(str(musicList[index]))
-            songLength = sound.info.length
-            songLength = round(songLength)
-            print(songLength)
-            min, sec = divmod(songLength, 60)
+            sound = MP3(str(muzikList[index]))
+            parcauzunluk = sound.info.length
+            parcauzunluk = round(parcauzunluk)
+            print(parcauzunluk)
+            min, sec = divmod(parcauzunluk, 60)
 
-            self.songLenthLabel.setText("/ " + str(min) + ":" + str(sec))
+            self.parcalabel.setText("/ " + str(min) + ":" + str(sec))
             self.progressBar.setValue(0)
-            self.progressBar.setMaximum(songLength)
-
+            self.progressBar.setMaximum(parcauzunluk)
         except:
             pass
 
-    def setVolume(self):
-        self.volume=self.volumeSlider.value()
-        # print(self.volume)
-        mixer.music.set_volume(self.volume/100)
+    def sesayarla(self):
+        self.sesayari=self.sesslider.value()
+        mixer.music.set_volume(self.sesayari/100)
 
-    def muteSound(self):
+    def sesikapat(self):
         global muted
 
         if muted == False:
             mixer.music.set_volume(0.0)
             muted = True
-            self.muteButton.setIcon(QIcon("icons/unmuted.png"))
-            self.muteButton.setToolTip("Ses aç")
-            self.volumeSlider.setValue(0)
+            self.sessiz.setIcon(QIcon("butonlar/unmuted.png"))
+            self.sessiz.setToolTip("Ses aç")
+            self.sesslider.setValue(0)
 
         else:
             mixer.music.set_volume(0.7)
             muted = False
-            self.muteButton.setToolTip("Sessiz")
-            self.muteButton.setIcon(QIcon("icons/mute.png"))
-            self.volumeSlider.setValue(70)
+            self.sessiz.setToolTip("Sessiz")
+            self.sessiz.setIcon(QIcon("butonlar/mute.png"))
+            self.sesslider.setValue(70)
 
-    def updateProgressBar(self):
-        global count
-        global songLength
-        count +=1
-        self.progressBar.setValue(count)
-        self.songTimerLabel.setText(time.strftime("%M:%S",time.gmtime(count)))
-        if count == songLength:
+    def progressbarguncelle(self):
+        global sayac
+        global parcauzunluk
+        sayac +=1
+        self.progressBar.setValue(sayac)
+        self.parcauzunluk.setText(time.strftime("%M:%S",time.gmtime(sayac)))
+
+        if sayac == parcauzunluk:
             self.timer.stop()
 
 
 
-def main():
-    App=QApplication(sys.argv)
-    window=Player()
-    sys.exit(App.exec_())
 
-if __name__ == '__main__':
-    main()
+App=QApplication(sys.argv)
+pencere=Player()
+sys.exit(App.exec_())
